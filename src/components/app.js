65b5ee52aa2c061759dc7cd7564d2347b78f41e0
Michael Ingleton <mingleton42@gmail.com>
2019-05-25 15:55:23 -0600

    generated project

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <div> Logo</div>
+        <h2>React Redux Router</h2>
+      </div>
+    );
+  }
+}
