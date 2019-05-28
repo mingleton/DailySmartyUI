f218bab532d19d07e51d2060d490f2204a9f83ca
Michael Ingleton <mingleton42@gmail.com>
2019-05-28 14:50:18 -0600

    created resultsPosts.js

--- src/components/home.js
+++ src/components/home.js
@@ -3,16 +3,28 @@ import Logo from "./logo";
 import SearchBar from "./searchbar.js";
 import RecentPosts from "./reacentPosts.js"
 
-export default class Home extends Component {
+import { connect } from 'react-redux';
+import * as actions from "../actions";
+
+class Home extends Component {
+
+
+  handleSearchBarSubmit(query) {
+    this.props.fetchPostsWithQuery(query);
+    this.props.history.push('/results');
+  }
+
   render() {
     return (
       <div className='app'>
         <div> 
           <Logo />
-          <SearchBar/>
+          <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
           <RecentPosts/>
         </div>
       </div>
     );
-  }
+  } 
 }
+
+export default connect(null, actions)(Home)
