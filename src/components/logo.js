18ff9fe93d6f824a4317c0f662a87cd4ffe29b71
Michael Ingleton <mingleton42@gmail.com>
2019-05-28 12:43:34 -0600

    added custom logo size to result.js

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 class Logo extends Component {
     render() {
+
+        const size = {
+            height:  this.props.size ? this.props.size : 105,
+            widht: this.props.size ? this.props.size : 105
+        }
         return (
             <div className="logo-main">
-                <img src="/assets/ds_circle_logo.png" />
+                <img style={size} alt="daily smarty ui image logo" src="/assets/ds_circle_logo.png" />
             </div>
         )
     }
