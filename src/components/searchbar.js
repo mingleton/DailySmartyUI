20c5f813c68dfa08de3f3b7fcd0315d2e6df0198
Michael Ingleton <mingleton42@gmail.com>
2019-05-28 17:26:24 -0600

    styled home and searchbar component

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -10,7 +10,7 @@ class SearchBar extends Component {
     }
 
     renderInput(field) {
-        return <input type="text" placeholder="Serach DailySmarty" {...field.input} />
+        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
     }
 
     render() {
@@ -19,7 +19,10 @@ class SearchBar extends Component {
 
         return (
            <form className = "search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-               <Field name= "query" component={this.renderInput}/>
+              <div className= "searchbar__wrapper">
+                    <Field name= "query" component={this.renderInput}/>
+                    <p>Press return to search</p>
+               </div>
            </form>
         )
     }
